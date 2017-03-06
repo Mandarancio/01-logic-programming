04a996e615c386f8a8442edca9f2459356a5824b
Alban Linard <alban@linard.fr>
2017-03-06 17:36:13 +0100

    Add examples.

--- Sources/___logic_programming.swift
+++ Sources/___logic_programming.swift
@@ -1,4 +1,44 @@
-struct ___logic_programming {
+//
+//  A LogicKit usage example that showcases the use of predicates.
+//
+//  Created by Dimitri Racordon on 12.02.17.
+//  Copyright © 2017 University of Geneva. All rights reserved.
+//
+import LogicKit
 
-    var text = "Hello, World!"
+
+// First, we create an enumeration to represent the different Pokemon.
+enum Person: Term {
+
+    case A, B, C, D, E, F, G, H, I
+
+    func equals(_ other: Term) -> Bool {
+        return (other is Person) && (other as! Person == self)
+    }
+
 }
+
+// We then define a set of predicates on the type of the pokemons.
+func is_student(who: Term) -> Goal {
+    return  (who === Person.A) ||
+            (who === Person.E) ||
+            (who === Person.I)
+}
+//
+// func fire(_ pokemon: Term) -> Goal {
+//     return  (pokemon ≡ Pokemon.Charmander) ||
+//             (pokemon ≡ Pokemon.Vulpix)
+// }
+//
+// func water(_ pokemon: Term) -> Goal {
+//     return  (pokemon ≡ Pokemon.Squirtle) ||
+//             (pokemon ≡ Pokemon.Psyduck)
+// }
+//
+// // We define another predicate that takes two Pokemon and holds when the first
+// // is stronger that ther second (solely based on their type).
+// func stronger(_ lhs: Term, _ rhs: Term) -> Goal {
+//     return  grass(lhs) && water(rhs) ||
+//             fire(lhs) && grass(rhs) ||
+//             grass(lhs) && water(rhs)
+// }
--- Tests/01-logic-programmingTests/___logic_programmingTests.swift
+++ Tests/01-logic-programmingTests/___logic_programmingTests.swift
@@ -1,11 +1,61 @@
 import XCTest
+import LogicKit
 @testable import ___logic_programming
 
 class ___logic_programmingTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(___logic_programming().text, "Hello, World!")
+        // We ask for all Pokemon that are stronger than Bulbasaur, according to our
+        // `stronger` predicate.
+        do {
+          let x = Variable(named: "x")
+          let q = is_student(who: x)
+          print("Which persons are students?")
+          let results = q(State())
+          for substitution in results {
+              print ("substitution found")
+              for (_, value) in substitution.reified() {
+                  print("* \(value)")
+              }
+          }
+        }
+        do {
+          let x = Variable(named: "x")
+          let y = Variable(named: "y")
+          let q = is_student(who: x) && is_student (who: y)
+          print("Which persons are students?")
+          let results = q(State())
+          for substitution in results {
+              print ("substitution found")
+              for (variable, value) in substitution.reified() {
+                  print("* \(variable) = \(value)")
+              }
+          }
+        }
+        do {
+          let q = is_student(who: Person.A)
+          print("Is A a student?")
+          let results = q(State())
+          for substitution in results {
+              print ("substitution found")
+              for (_, value) in substitution.reified() {
+                  print("* \(value)")
+              }
+          }
+        }
+        do {
+          let q = is_student(who: Person.B)
+          print("Is B a student?")
+          let results = q(State())
+          for substitution in results {
+              print ("substitution found")
+              for (_, value) in substitution.reified() {
+                  print("* \(value)")
+              }
+          }
+        }
+        // XCTAssertEqual("Hello, World!", "Hello, World!")
     }
 
 
